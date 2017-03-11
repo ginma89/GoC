77601d89765606b2a5e1f011742250c05664b8f6
Maxime PHILIPPOT <maximephilippot@ext.sigi.lu>
2017-03-11 22:22:12 +0100

    Import

--- integration/js/import.js
+++ integration/js/import.js
@@ -1,16 +1,10 @@
-﻿jQuery(document).ready(function () {
-    importData();
-
 var communesdata = {};
-
-
 var files = [
-    "https://raw.githubusercontent.com/ginma89/GoC/master/data/json/accidents.json",
-    "https://raw.githubusercontent.com/ginma89/GoC/master/data/json/dechetsMenagers.json"
+    "https://raw.githubusercontent.com/ginma89/GoC/master/data/json/accidents.json"
+    , "https://raw.githubusercontent.com/ginma89/GoC/master/data/json/dechetsMenagers.json"
 ];
 
 function importData() {
-
     for (var i = 0; i < communelist.length; i++) {
         for (var j = 0; j < files.length; j++) {
             $.getJSON(files[j], function (data) {
@@ -18,7 +12,7 @@ function importData() {
             });
         }
     };
-
 }
-
+jQuery(document).ready(function () {
+    importData();
 })
