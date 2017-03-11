dd5f2eb26a44d861595bb7de263a2e4ce9fa573e
Maxime PHILIPPOT <maximephilippot@ext.sigi.lu>
2017-03-11 20:01:43 +0100

    Updated display

--- integration/js/goc.js
+++ integration/js/goc.js
@@ -1,17 +1,18 @@
     function svgSizer() {
         var svgParentW = $('svg').parent().width();
         console.log(svgParentW)
-        $('svg').css('transform', 'scale(1.0)')
         var svgW = $('svg').width();
         var svgratio = svgParentW / svgW;
+        var svgRespW = svgW * svgratio;
         console.log(svgratio);
-        $('svg').css('transform', 'scale(' + svgratio + ')')
+        $('svg').attr('height', svgParentW)
         $('svg').css('visibility', 'visible');
-        var relX = $('.map').position().left;
-        var relY = $('.map').position().top;
-        $('svg').css('top', relY);
-        $('svg').css('top', relX);
     }
     jQuery(document).ready(function () {
-    svgSizer();
+        svgSizer();
+        /*** Commune Selected ***/
+        $('path').on('click', function () {
+            $('path').removeAttr('selected');
+            $(this).attr('selected', 'true')
+        })
     })
