c124a5ef480add3b6b7b5ab7010f69fa4f3079fe
Maxime PHILIPPOT <maximephilippot@ext.sigi.lu>
2017-03-11 18:57:13 +0100

    Update

--- integration/js/goc.js
+++ integration/js/goc.js
@@ -1,1 +1,17 @@
- 
+    function svgSizer() {
+        var svgParentW = $('svg').parent().width();
+        console.log(svgParentW)
+        $('svg').css('transform', 'scale(1.0)')
+        var svgW = $('svg').width();
+        var svgratio = svgParentW / svgW;
+        console.log(svgratio);
+        $('svg').css('transform', 'scale(' + svgratio + ')')
+        $('svg').css('visibility', 'visible');
+        var relX = $('.map').position().left;
+        var relY = $('.map').position().top;
+        $('svg').css('top', relY);
+        $('svg').css('top', relX);
+    }
+    jQuery(document).ready(function () {
+    svgSizer();
+    })
